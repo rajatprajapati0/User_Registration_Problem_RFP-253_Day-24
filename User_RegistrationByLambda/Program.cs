03da33005e6d086c550165782f75dc4b90c3921b
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-22 01:29:51 +0530

    Added: User Registration program

---
+++ User_RegistrationByLambda/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace User_RegistrationByLambda 
+{
+  public class Program
+  {
+        public static void Main(string[] args) 
+        {
+
+            Console.WriteLine(" welcome User_Registration Program");
+         
+        
+        
+        }
+  }
+
+}
